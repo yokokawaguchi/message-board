546512a11f134f7cf93ce1cf80fc1a9089a175dd
yoko <yoko.cookiemonster718@gmail.com>
2017-04-25 02:47:49 +0000

    DRY

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+     root to: 'messages#index'
+
+  resources :messages
+    #For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    #CRUD
+    get 'messages/:id' ,to:'messages#show'
+    post 'messages',to:'messages#create'
+    put 'messages/:id', to: 'messages#update'
+    delete 'messages/:id', to: 'messages#destroy'
+    # index: show の補助ページ
+    get 'messages', to: 'messages#index'
+    # new: 新規作成用のフォームページ
+    get 'messages/new', to: 'messages#new'
+    # edit: 更新用のフォームページ
+    get 'messages/:id/edit', to: 'messages#edit'
 end
